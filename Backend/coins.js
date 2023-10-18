0e2275d575e83f6a03a8e4ddb2f1399692ec1bad
Daniel Gomes <danielgjjpatrocinio@gmail.com>
2023-10-18 18:46:22 -0300

    First commit

---
+++ Backend/coins.js
@@ -0,0 +1,12 @@
+export const coins = [
+  { "name": "Bitcoin", "code": "BTCUSDT" },
+  { "name": "Ethereum", "code": "ETHUSDT" },
+  { "name": "Binance Coin", "code": "BNBUSDT" },
+  { "name": "Cardano", "code": "ADAUSDT" },
+  { "name": "Solana", "code": "SOLUSDT" },
+  { "name": "XRP", "code": "XRPUSDT" },
+  { "name": "Polkadot", "code": "DOTUSDT" },
+  { "name": "Dogecoin", "code": "DOGEUSDT" },
+  { "name": "Chainlink", "code": "LINKUSDT" },
+  { "name": "Litecoin", "code": "LTCUSDT" }
+]
